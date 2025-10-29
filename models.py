a1c1298af8dbb74c58083e134ad26b90d25c5ec0
Vanessa Watson <vanessawatson@Vanessas-MacBook-Pro.local>
2025-10-28 19:09:19 -0700

    Added defined models for deck and cards

---
+++ models.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Deck(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    cards = db.relationship('Card', backref='deck', lazy=True)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    deck_id = db.Column(db.Integer, db.ForeignKey('deck.id'), nullable=False)
+    front_text = db.Column(db.String(255), nullable=False)
+    back_text = db.Column(db.String(255), nullable=False)
+    order_index = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
