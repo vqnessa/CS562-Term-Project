0383a018dc60dfc6537551b0f08f64f4e5228ef2
Vanessa Watson <vanessawatson@Vanessas-MacBook-Pro.local>
2025-11-17 12:06:22 +0100

    Fixed bugs in database model, e.g. the date showing a day ahead

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 from app import db
+from datetime import datetime
 
 class Deck(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(
+        db.DateTime, default=datetime.now(), onupdate=datetime.now())
     cards = db.relationship('Card', backref='deck', lazy=True)
 
 class Card(db.Model):
@@ -13,5 +15,6 @@ class Card(db.Model):
     front_text = db.Column(db.String(255), nullable=False)
     back_text = db.Column(db.String(255), nullable=False)
     order_index = db.Column(db.Integer)
-    created_at = db.Column(db.DateTime, default=db.func.now())
-    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(
+        db.DateTime, default=datetime.now(), onupdate=datetime.now())
